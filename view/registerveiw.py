3c054256c4ccac22846b6baeeb5df36e7a7e40c8
wangyi1310 <1310874029@qq.com>
2018-05-27 00:02:41 +0800

    Update registerveiw.py

--- view/registerveiw.py
+++ view/registerveiw.py
@@ -12,22 +12,22 @@ class RegisterView(view.View):
         :param message:
         :return:
         """
-		user_name=message['user_name']
-		resluts = self.mongo.find_user_info(user_name)
-		self.clientfd=message['clientfd']
-		if len(resluts):
-			self.code = 2
-			self.state = True
-			return 
-		self.mongo.insert_user_info(message)
-		self.code = 0
+	user_name=message['user_name']
+	resluts = self.mongo.find_user_info(user_name)
+	self.clientfd=message['clientfd']
+	if len(resluts):
+		self.code = 2
 		self.state = True
+		return 
+	self.mongo.insert_user_info(message)
+	self.code = 0
+	self.state = True
 		
     def response(self):
         if self.state:
-			message = "REGISTER  SUCCESS"
-		else:
-			message = "REGISTER FAILUER"
-		data={'code',self.code,'message':message}
-		data=json.dumps(data)
-		s.sock_dict[self.clientfd].send(data)
+		message = "REGISTER  SUCCESS"
+	else:
+		message = "REGISTER FAILUER"
+	data={'code',self.code,'message':message}
+	data=json.dumps(data)
+	s.sock_dict[self.clientfd].send(data)
